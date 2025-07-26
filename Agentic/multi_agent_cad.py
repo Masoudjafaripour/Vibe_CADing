30c62df07b848344f417e5708a2c280653ea4ba6
Masoud Jafaripour <mjafaripoor110@gmail.com>
2025-07-25 21:51:05 -0600

    agentic pipline was started

---
+++ Agentic/multi_agent_cad.py
@@ -0,0 +1,6 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+
+llm = ChatOpenAI(model="gpt-4", temperature=0)
